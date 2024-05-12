0cb43f4fdee90ad52edbf0ac599090354aca5aab
Lorenzo <lorenzo@nioel.com>
2024-05-13 03:15:38 +1000

    feat: init sdk

---
+++ sdk/index.ts
@@ -0,0 +1,36 @@
+export interface Message {
+  content: string;
+  timestampUnix: number;
+}
+
+export class PluginOutput {
+  private name: string;
+  private data: Map<string, string> | null;
+  private systemMessage: string | null;
+  private assistantMessage: string | null;
+
+  public constructor(name: string) {
+    this.name = name;
+    this.data = null;
+    this.systemMessage = null;
+    this.assistantMessage = null;
+  }
+
+  public withData(key: string, value: string): PluginOutput {
+    if (!this.data) {
+      this.data = new Map();
+    }
+    this.data.set(key, value);
+    return this;
+  }
+
+  public withSystemMessage(message: string): PluginOutput {
+    this.systemMessage = message;
+    return this;
+  }
+
+  public withAssistantMessage(message: string): PluginOutput {
+    this.assistantMessage = message;
+    return this;
+  }
+}
