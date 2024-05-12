dcc348e0c4c80b0c8fe8864edc0df2b9c4d52183
Lorenzo <lorenzo@nioel.com>
2024-05-13 03:15:49 +1000

    feat: init example plugin

---
+++ examples/hello-world-plugin/index.ts
@@ -0,0 +1,5 @@
+import {Message, PluginOutput} from "@asterai-io/sdk";
+
+export function processMessage(input: Message): PluginOutput | null {
+  return new PluginOutput("default").withAssistantMessage("Hello, world!");
+}
