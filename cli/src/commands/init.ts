a1d98b287e158b181359063a2f3f7d9262be6921
Lorenzo <lorenzo@nioel.com>
2024-06-16 20:53:06 +1000

    feat: init command

--- cli/src/commands/init.ts
+++ cli/src/commands/init.ts
@@ -1,76 +1,28 @@
-import { Command, Flags } from "@oclif/core";
+import { Args, Command } from "@oclif/core";
 import path from "path";
-import YAML from "yaml";
 import fs from "fs";
-import { WriteStream } from "node:fs";
+import url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const SOURCE_DIR = path.join(__dirname, "../../asterai-init-plugin");
 
 export default class Codegen extends Command {
-  static args = {};
-  static description = "Create a new plugin project";
-
-  static examples = [`<%= config.bin %> <%= command.id %>`];
-
-  static flags = {
-    manifest: Flags.string({
-      char: "m",
-      description: "manifest path",
-      default: "plugin.asterai.yaml",
-    }),
-    outputDir: Flags.string({
-      char: "o",
-      description: "output directory",
-      default: "generated",
+  static args = {
+    outDir: Args.string({
+      default: "plugin",
     }),
   };
 
+  static description = "Initialise a new plugin project";
+
+  static examples = [`<%= config.bin %> <%= command.id %> project-name`];
+
+  static flags = {};
+
   async run(): Promise<void> {
-    const { flags } = await this.parse(Codegen);
-    const manifestPath = path.resolve(flags.manifest);
-    const baseDir = path.dirname(manifestPath);
-    const outDir = path.join(baseDir, flags.outputDir);
-    const manifest = ManifestSchema.parse(
-      YAML.parse(fs.readFileSync(manifestPath, "utf8")),
-    );
-    if (!fs.existsSync(outDir)) {
-      fs.mkdirSync(outDir, { recursive: true });
-    }
-    for (const func of manifest.functions) {
-      const className = `${capitaliseFirstLetter(func.name)}Args`;
-      const fileName = `${className}.ts`;
-      const filePath = path.join(outDir, fileName);
-      const stream = fs.createWriteStream(filePath);
-      writeFunctionFile(className, func.arguments, stream);
-    }
+    const { args } = await this.parse(Codegen);
+    const outDir = path.resolve(args.outDir);
+    fs.cpSync(SOURCE_DIR, outDir, { recursive: true });
   }
 }
-
-const writeFunctionFile = (
-  className: string,
-  args: ManifestFunctionArgumentSchema[],
-  stream: WriteStream,
-) => {
-  stream.write(`\
-import {parseTypeFromString, TypedMap} from "@asterai-io/sdk/collections";
-
-export class ${className} extends TypedMap<string, string> {
-  ${args.map(renderArgGetterString).join("")}
-}    
-  `);
-};
-
-const renderArgGetterString = (arg: ManifestFunctionArgumentSchema): string =>
-  `
-  public get ${arg.name}(): ${arg.type} {
-    ${
-      arg.type === "string"
-        ? `return this.mustGet("${arg.name}")`
-        : `
-    const value = this.mustGet("${arg.name}");
-    return parseTypeFromString<${arg.type}>(value, "${arg.type}");
-        `.trim()
-    }
-  }
-  `;
-
-const capitaliseFirstLetter = (string: string) =>
-  string.charAt(0).toUpperCase() + string.slice(1);
