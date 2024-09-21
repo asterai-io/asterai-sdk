e0b53e58eeaa5adf4ab40073b993c24d0eb64cfc
Lorenzo Fontoura <lorenzo@nioel.com>
2024-09-21 18:47:44 +1000

    refactor!: codegen with protoc

--- cli/src/commands/codegen.ts
+++ cli/src/commands/codegen.ts
@@ -1,51 +1,10 @@
 import { Command, Flags } from "@oclif/core";
 import path from "path";
-import z from "zod";
-import YAML from "yaml";
 import fs from "fs";
-import { WriteStream } from "node:fs";
+import { execSync } from "node:child_process";
 
-const ASC_TYPES: readonly [string, ...string[]] = [
-  "i8",
-  "u8",
-  "i16",
-  "u16",
-  "i32",
-  "u32",
-  "i64",
-  "u64",
-  "isize",
-  "usize",
-  "f32",
-  "f64",
-  "bool",
-  "string",
-];
-
-const ManifestFunctionArgumentSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  type: z.enum(ASC_TYPES),
-});
-
-type ManifestFunctionArgumentSchema = z.infer<
-  typeof ManifestFunctionArgumentSchema
->;
-
-const ManifestFunctionSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  arguments: z.array(ManifestFunctionArgumentSchema).optional(),
-});
-
-type ManifestFunctionSchema = z.infer<typeof ManifestFunctionSchema>;
-
-const ManifestSchema = z.object({
-  name: z.string(),
-  functions: z.array(ManifestFunctionSchema).optional(),
-});
-
-type ManifestSchema = z.infer<typeof ManifestSchema>;
+const AS_PROTO_GEN_PATH =
+  "./node_modules/@asterai/sdk/node_modules/.bin/as-proto-gen";
 
 export default class Codegen extends Command {
   static args = {};
@@ -57,7 +16,7 @@ export default class Codegen extends Command {
     manifest: Flags.string({
       char: "m",
       description: "manifest path",
-      default: "plugin.asterai.yaml",
+      default: "plugin.asterai.proto",
     }),
     outputDir: Flags.string({
       char: "o",
@@ -71,52 +30,32 @@ export default class Codegen extends Command {
     const manifestPath = path.resolve(flags.manifest);
     const baseDir = path.dirname(manifestPath);
     const outDir = path.join(baseDir, flags.outputDir);
-    const manifest = ManifestSchema.parse(
-      YAML.parse(fs.readFileSync(manifestPath, "utf8")),
-    );
-    if (!manifest.functions) {
-      return;
-    }
     if (!fs.existsSync(outDir)) {
       fs.mkdirSync(outDir, { recursive: true });
+    } else {
+      deleteOldGeneratedFiles(outDir);
     }
-    for (const func of manifest.functions) {
-      const className = `${capitaliseFirstLetter(func.name)}Args`;
-      const fileName = `${className}.ts`;
-      const filePath = path.join(outDir, fileName);
-      const stream = fs.createWriteStream(filePath);
-      writeFunctionFile(className, func.arguments ?? [], stream);
+    try {
+      execSync(
+        "protoc " +
+          `--plugin=protoc-gen-as=${AS_PROTO_GEN_PATH} ` +
+          "--as_opt=gen-helper-methods " +
+          `--as_out=./${flags.outputDir} ./${flags.manifest}`,
+      );
+    } catch (e) {
+      console.error("Failed to generate protobuf types:", e);
     }
   }
 }
 
-const writeFunctionFile = (
-  className: string,
-  args: ManifestFunctionArgumentSchema[],
-  stream: WriteStream,
-) => {
-  stream.write(`\
-import {parseTypeFromString, TypedMap} from "@asterai/sdk/collections";
-
-export class ${className} extends TypedMap<string, string> {
-  ${args.map(renderArgGetterString).join("")}
-}    
-  `);
-};
-
-const renderArgGetterString = (arg: ManifestFunctionArgumentSchema): string =>
-  `
-  public get ${arg.name}(): ${arg.type} {
-    ${
-      arg.type === "string"
-        ? `return this.mustGet("${arg.name}")`
-        : `
-    const value = this.mustGet("${arg.name}");
-    return parseTypeFromString<${arg.type}>(value, "${arg.type}");
-        `.trim()
+const deleteOldGeneratedFiles = (outDir: string) => {
+  const oldFiles = fs.readdirSync(outDir);
+  for (const oldFile of oldFiles) {
+    const file = path.parse(oldFile);
+    if (file.ext !== ".ts") {
+      continue;
     }
+    const deletePath = path.join(outDir, oldFile);
+    fs.unlinkSync(deletePath);
   }
-  `;
-
-const capitaliseFirstLetter = (string: string) =>
-  string.charAt(0).toUpperCase() + string.slice(1);
+};
