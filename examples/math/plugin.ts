be61d749f7b38498a4e5430a4aee63e4223ed53d
Lorenzo Fontoura <lorenzo@nioel.com>
2024-09-23 00:23:21 +1000

    fix: ptr handling

--- examples/math/plugin.ts
+++ examples/math/plugin.ts
@@ -1,18 +1,21 @@
 import { BinaryOperationInput } from "./generated/BinaryOperationInput";
 import { CalculationOutput } from "./generated/CalculationOutput";
 import { Protobuf } from "@asterai/as-proto/assembly";
+import { readBufferFromPtr, writeBufferToPr } from "@asterai/sdk/protobuf";
 export * from "@asterai/sdk/exports";
 
-export function add(inputBuffer: Uint8Array): Uint8Array {
+export function add(ptr: u32): u32 {
+  const inputBuffer = readBufferFromPtr(ptr);
   const input = Protobuf.decode<BinaryOperationInput>(
     inputBuffer,
     BinaryOperationInput.decode,
   );
   const result = input.a + input.b;
-  return Protobuf.encode<CalculationOutput>(
-    new CalculationOutput(result),
+  const outputBuffer = Protobuf.encode<CalculationOutput>(
+    new CalculationOutput(result, `the result is ${result}`),
     CalculationOutput.encode,
   );
+  return writeBufferToPr(outputBuffer);
 }
 
 export function mul(input: BinaryOperationInput): CalculationOutput {
