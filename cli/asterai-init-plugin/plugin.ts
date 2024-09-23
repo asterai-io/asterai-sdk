89ea2ab7e7f66a24e04c12b4550e017623ec3dfb
Lorenzo Fontoura <lorenzo@nioel.com>
2024-09-23 10:27:55 +1000

    chore: update init example

--- cli/asterai-init-plugin/plugin.ts
+++ cli/asterai-init-plugin/plugin.ts
@@ -1,7 +1,35 @@
-import { OrderBurgerArgs } from "./generated/OrderBurgerArgs";
-export * from "@asterai/sdk/exports";
+import { BinaryOperationInput } from "./generated/BinaryOperationInput";
+import { CalculationOutput } from "./generated/CalculationOutput";
+import { PluginContext } from "./generated/PluginContext";
+import { ProcessQueryOutput } from "./generated/ProcessQueryOutput";
+import { Log } from "@asterai/sdk";
 
-export function orderBurger(args: OrderBurgerArgs): string {
-  // TODO: make http call to burger API.
-  return `burger delivered to ${args.address}`;
+export function processQuery(input: PluginContext): ProcessQueryOutput {
+  Log.info(`math plugin received a query: ${input.query.content}`);
+  // This doesn't return any data, but protobuf requires functions
+  // to always have one input and one output exactly to ensure
+  // backward compatibility.
+  return new ProcessQueryOutput();
+}
+
+export function add(input: BinaryOperationInput): CalculationOutput {
+  const result = input.a + input.b;
+  // CalculationOutput returns a system message.
+  // The `system_message` field is sent to the LLM.
+  return new CalculationOutput(`the result is ${result}`);
+}
+
+export function mul(input: BinaryOperationInput): CalculationOutput {
+  const result = input.a * input.b;
+  return new CalculationOutput(`the result is ${result}`);
+}
+
+export function div(input: BinaryOperationInput): CalculationOutput {
+  const result = input.a / input.b;
+  return new CalculationOutput(`the result is ${result}`);
+}
+
+export function pow(input: BinaryOperationInput): CalculationOutput {
+  const result = input.a ** input.b;
+  return new CalculationOutput(`the result is ${result}`);
 }
