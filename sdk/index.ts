7540de68e20a7429f6b98fddd9b738cad0e043a6
Lorenzo Fontoura <lorenzo@nioel.com>
2024-09-23 10:30:02 +1000

    feat: new logging interface wrapper

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,9 +1,42 @@
-import { Uint8Array } from "typedarray";
+import { writeBufferToPr } from "./buffer";
+import { Protobuf } from "@asterai/as-proto/assembly";
+import { HostLogRequest } from "./generated/HostLogRequest";
 
-export declare namespace host {
-  export function log(request: Uint8Array): void;
-  export function http_request(request: Uint8Array): Uint8Array;
-  export function semantic_search(request: Uint8Array): Uint8Array;
-  export function kv_get_user_string(request: Uint8Array): Uint8Array;
-  export function kv_set_user_string(request: Uint8Array): void;
+declare namespace host {
+  export function log(request: u32): void;
+  export function http_request(request: u32): u32;
+  export function semantic_search(request: u32): u32;
+  export function kv_get_user_string(request: u32): u32;
+  export function kv_set_user_string(request: u32): void;
+}
+
+export class Log {
+  public static trace(content: string) {
+    Log.level(content, "trace");
+  }
+
+  public static debug(content: string) {
+    Log.level(content, "debug");
+  }
+
+  public static info(content: string) {
+    Log.level(content, "info");
+  }
+
+  public static warn(content: string) {
+    Log.level(content, "warn");
+  }
+
+  public static error(content: string) {
+    Log.level(content, "error");
+  }
+
+  static level(content: string, level: string) {
+    const request = new HostLogRequest(content, level);
+    const encoded = Protobuf.encode<HostLogRequest>(
+      request,
+      HostLogRequest.encode,
+    );
+    host.log(writeBufferToPr(encoded));
+  }
 }
