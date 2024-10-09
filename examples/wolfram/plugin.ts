ffedac7eba287ee572a3e1b19568fa7d757c472e
Raphael Oliveira <raphael.antonio.oliveira@gmail.com>
2024-10-09 04:17:31 -0300

    feat: add wolfram as an example plugin that uses envs

---
+++ examples/wolfram/plugin.ts
@@ -0,0 +1,28 @@
+import { Log, PluginEnvStorage } from "@asterai/sdk";
+import { TextQueryRequest } from "./generated/TextQueryRequest";
+import { TextQueryResponse } from "./generated/TextQueryResponse";
+import { HttpRequestBuilder } from "@asterai/sdk";
+
+export function textQuery(args: TextQueryRequest): TextQueryResponse {
+  const envStorage = new PluginEnvStorage();
+  const appId = envStorage.getString("appId");
+
+  if (!appId) {
+    return new TextQueryResponse(`
+      It looks like you haven't set your Wolfram Alpha app id. Please set it in the plugin settings as:
+      APP_ID=<your app id>
+      You can get an app id by signing up at https://developer.wolframalpha.com/portal/myapps
+    `);
+  }
+
+  Log.debug(`Received text query: ${args.query}`);
+  let response = new HttpRequestBuilder("api.wolframalpha.com")
+  .method("GET")
+  .path(`/v1/result`)
+  .query("appid", appId)
+  .query("i", args.query)
+  .build()
+  .send();
+
+  return new TextQueryResponse(`According to Wolfram Alpha, ${args.query} is ${response.response}.`);
+}
